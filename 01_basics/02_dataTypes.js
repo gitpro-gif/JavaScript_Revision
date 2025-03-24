e3c6eda517f15e45ae3b03885e31a475fb4369b3
gitpro-gif <vedantgithub2@gmail.com>
2025-03-24 19:24:08 +0530

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,12 @@
+let name = "Vedant"
+let age = 21
+let isLogin = true
+
+// number => 2 to poewr 53
+//bigint
+
+console.log(typeof(name));
+console.log(typeof(age));
+console.log(typeof(isLogin));
+
+
