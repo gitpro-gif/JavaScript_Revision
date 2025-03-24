706a0d65df31bdb6aadfc5724b6040f9ffd92145
gitpro-gif <vedantgithub2@gmail.com>
2025-03-24 19:06:23 +0530

    Variables done

---
+++ 01_basics/variable.js
@@ -0,0 +1,15 @@
+const accountId = 1445533
+let accountEmail = "abc@gmail.com"
+var accountPassword = "1234"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2
+accountEmail = "hc@gmail.com"
+accountPassword = "6789"
+accountCity = "Beng"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
